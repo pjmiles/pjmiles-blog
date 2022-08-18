313bc8d98651318a447d198dc650fb071730c881
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-08-18 17:59:11 +0100

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <div className="nav-bar">
+            <div className="logo">PJBLOG</div>
+
+            <div className="nav-item">
+                <span className="nav-link">Home</span>
+                <span className="nav-link">New Blog</span>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
