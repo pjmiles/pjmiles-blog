6c104625393c28ca39235097d48a5b96ea89e765
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-08-23 18:23:11 +0100

    footer section added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Blogs from "./components/Blogs";
 import CreateBlog from "./components/CreateBlog";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Blogs />} />
-          <Route path="/create" element={<CreateBlog />}/>
-        </Routes>
+      <Routes>
+        <Route exact path="/" element={<Blogs />} />
+        <Route path="/create" element={<CreateBlog />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
