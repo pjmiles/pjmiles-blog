71e95800d6bfd510184e69e2123189bc290d849f
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-11-21 17:34:09 +0100

    header component

---
+++ src/components/Heading.js
@@ -0,0 +1,15 @@
+const Heading = () => {
+  return (
+    <header className="heading">
+      <h1 className="bold-text">
+        This is a place <br /> to write, read and <br /> connect.
+      </h1>
+      <p className="p-text">
+        It's easy and free to post on any topic and <br /> connect with millions
+        of readers.
+      </p>
+    </header>
+  );
+};
+
+export default Heading;
